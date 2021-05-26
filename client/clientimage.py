198e076ab1115c1ed8f1601143134ae25a18ca53
kaefd <70887308+kaefd@users.noreply.github.com>
2021-05-26 16:00:18 +0700

    update url

--- client/clientimage.py
+++ client/clientimage.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("http://127.0.0.1:5000/predict", files={"file": open('wallbg.jpg', 'rb')})
+resp = requests.post("https://olahcitra-1.herokuapp.com/predict", files={"file": open('wallbg.jpg', 'rb')})
 
 print(resp.json())
